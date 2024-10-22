de80f734c234990c842ce662d42b6ec41f4b3783
Rolande carlene <rolandenishimwe@gmail.com>
2024-10-22 20:27:58 +0200

    Reregister the rest model in admin.py

--- Property_Management/Property_App/admin.py
+++ Property_Management/Property_App/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Property,Unit
+from .models import Property,Unit,Tenant,Lease
 
 admin.site.register(Property)
 admin.site.register(Unit)
+admin.site.register(Tenant)
+admin.site.register(Lease)
 
 
 
