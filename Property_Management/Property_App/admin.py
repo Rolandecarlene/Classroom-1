9c4f2bdbd515a14a2cf43e50cf94257614e67b72
Rolande carlene <rolandenishimwe@gmail.com>
2024-10-22 20:20:35 +0200

    deregister registered modules in admin

--- Property_Management/Property_App/admin.py
+++ Property_Management/Property_App/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Property,Unit,Tenant,Lease
+from .models import Property,Unit
 
 admin.site.register(Property)
 admin.site.register(Unit)
-admin.site.register(Tenant)
-admin.site.register(Lease)
+
 
 
